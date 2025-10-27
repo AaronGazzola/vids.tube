48cb722025dfb86e6750436cbee2718c49edc7d0
Az Anything <azanything@Arons-Mac-mini.local>
2025-10-28 10:10:26 +1100

    fix(build): duplicate schema for worker to generate prisma client

--- worker/src/processor.ts
+++ worker/src/processor.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../lib/generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 import path from "path";
 import os from "os";
 import ffmpeg from "fluent-ffmpeg";
